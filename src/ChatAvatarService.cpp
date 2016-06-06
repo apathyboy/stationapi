ff1c05b9209aef5077bdae63a06bbe6d172abb67
apathy <ap@eb2.co>
2016-06-06 10:32:22 -0700

    Add stubs for getting avatars from the online list and from persistent storage

--- src/ChatAvatarService.cpp
+++ src/ChatAvatarService.cpp
@@ -1,11 +1,37 @@
 #include "ChatAvatarService.hpp"
+#include "ChatAvatar.hpp"
 
 #include "ServiceContainer.hpp"
 
 ChatAvatarService::ChatAvatarService(SwgChatConfig* config)
     : config_{config} {}
 
+ChatAvatarService::~ChatAvatarService() {}
+
 ChatAvatar* ChatAvatarService::GetAvatarByNameAndAddress(
+    const std::wstring& name, const std::wstring& address) {
+    ChatAvatar* avatar = GetOnlineAvatarByNameAndAddress(name, address);
+
+    if (!avatar) {
+        avatar = GetPersistedAvatarByNameAndAddress(name, address);
+    }
+
+    return avatar;
+}
+
+ChatAvatar* ChatAvatarService::GetOnlineAvatarByNameAndAddress(
+    const std::wstring& name, const std::wstring& address) {
+    ChatAvatar* avatar = nullptr;
+
+    auto find_iter = avatarMap_.find(name + L"+" + address);
+    if (find_iter != std::end(avatarMap_)) {
+        avatar = find_iter->second.get();
+    }
+
+    return avatar;
+}
+
+ChatAvatar* ChatAvatarService::GetPersistedAvatarByNameAndAddress(
     const std::wstring& name, const std::wstring& address) {
     return nullptr;
 }
--- src/ChatAvatarService.hpp
+++ src/ChatAvatarService.hpp
@@ -1,7 +1,9 @@
 
 #pragma once
 
+#include <memory>
 #include <string>
+#include <unordered_map>
 
 struct ChatAvatar;
 struct SwgChatConfig;
@@ -9,9 +11,14 @@ struct SwgChatConfig;
 class ChatAvatarService {
 public:
     explicit ChatAvatarService(SwgChatConfig* config);
+    ~ChatAvatarService();
 
     ChatAvatar* GetAvatarByNameAndAddress(const std::wstring& name, const std::wstring& address);
+    ChatAvatar* GetOnlineAvatarByNameAndAddress(const std::wstring& name, const std::wstring& address);
+    ChatAvatar* GetPersistedAvatarByNameAndAddress(const std::wstring& name, const std::wstring& address);
 
 private:
     SwgChatConfig* config_;
+
+    std::unordered_map<std::wstring, std::unique_ptr<ChatAvatar>> onlineAvatars_;
 };
